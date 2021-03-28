8e371f634379c1957e7e6c56f9b1dcb5c7b0cc6a
porot07 <ciscomalware@github.com>
2021-03-28 23:51:45 +0500

    feat: add solution carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,5 +2,11 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
+  if (isNaN(sampleActivity) || typeof sampleActivity !== 'string' || +sampleActivity <= 0 || +sampleActivity > 15) return false;
   
+  const doubleNumber = parseFloat(sampleActivity);
+  const log = 0.693 / HALF_LIFE_PERIOD;
+  const total = Math.ceil((Math.log(MODERN_ACTIVITY / doubleNumber)) / log);
+
+  return total;
 };
